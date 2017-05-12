c1f2daa3b6e547804cf1a2fe1d9b990520c37d24
Ken <Ken@Ken-PC.home>
2017-05-12 08:17:11 -0400

    Initial Commit

---
+++ src/aura/campingList/campingListController.js
@@ -0,0 +1,65 @@
+({
+
+    clickCreateCampingItem: function(component, event, helper) {
+
+        // Simplistic error checking
+        var isValid = true;
+
+        // Name must not be blank
+        var nameField = component.find("itemname");
+        var itemname = nameField.get("v.value");
+        if ($A.util.isEmpty(itemname)){
+            isValid = false;
+            nameField.set("v.errors", [{message:"Camping item name can't be blank."}]);
+        }
+        else {
+            nameField.set("v.errors", null);
+        }
+
+        // Price must not be blank
+        var priceField = component.find("price");
+        var itemprice = priceField.get("v.value");
+        if ($A.util.isEmpty(itemprice)){
+            isValid = false;
+            priceField.set("v.errors", [{message:"Camping item price can't be blank."}]);
+        }
+        else {
+            priceField.set("v.errors", null);
+        }
+
+        // Quantity must not be blank
+        var quantityField = component.find("quantity");
+        var itemquantity = quantityField.get("v.value");
+        if ($A.util.isEmpty(itemprice)){
+            isValid = false;
+            quantityField.set("v.errors", [{message:"Camping item quantity can't be blank."}]);
+        }
+        else {
+            quantityField.set("v.errors", null);
+        }
+
+
+        // If we pass error checking, do some real work
+        if (isValid) {
+            
+            // Create the new Camping Item
+            var items = component.get("v.items");
+            var newItem = component.get("v.newItem");
+            console.log("Create item: " + JSON.stringify(newItem));
+
+            // THIS IS A DISGUSTING, TEMPORARY HACK
+            var newItemIns = JSON.parse(JSON.stringify(newItem));
+
+            items.push(newItemIns);
+            component.set("v.items", items);     
+
+            component.set("v.newItem",
+                          {'sobjectType':'Camping_Item__c',
+                			'Name': '',
+                			'Quantity__c': 0,
+                			'Price__c': 0,
+                			'Packed__c': false});
+            
+        }
+    }
+})
